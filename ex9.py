cc050f2f9245127d3a4f38056c657fc1d4c01c4d
Silas Burger <silas.a.burger@gmail.com>
2018-08-02 12:00:46 -0700

    Finished ex10

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days = "Mon Tues Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+# Adding triple quotes allows spacing to be recorded without the \n character
+print("""
+There is something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
