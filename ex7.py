392cf6546238733c2188639c26e2009d4aa72ef1
Silas Burger <silas.a.burger@gmail.com>
2018-08-02 10:04:56 -0700

    Finished ex7

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb.") # Prints a string
+print("It's fleece was white as {}.".format("snow")) # Calls format on string in print function. This works the same as the previous exercise.
+
+print("And everywhere that Mary went.") # Prints a string
+print("." * 10) # Prints the string times 10
+
+end1 = "C" # Sets variable to different strings
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ') # Prints concatinated strings then adds a space to the end. The end variable didn't exist so it must be special. The comma denotes a new part or expression in the print function.
+print(end7 + end8 + end9 + end10 + end11 + end12) # Concatinates the rest of the end variables.
