2ecb4fe1961eac9b4b338e8884f36383601ec930
Silas Burger <silas.a.burger@gmail.com>
2018-08-02 15:23:14 -0700

    Finished ex11

--- ex11.py
+++ ex11.py
@@ -8,3 +8,6 @@ print('How much do you weigh?')
 weight = input('ENTER WEIGHT IN POUNDS:');
 
 print(f'So, you\'re {age} old, {height} feet tall, and pounds {weight} heavy.')
+
+emotion = input('Type how you are feeling in one word: ')
+print(f'So, you are feeling {emotion}.')
