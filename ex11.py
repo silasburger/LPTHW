3c1754b360922a8b44a0365c529f4b78d372b086
Silas Burger <silas.a.burger@gmail.com>
2018-08-02 15:29:35 -0700

    Finally Finished ex11

--- ex11.py
+++ ex11.py
@@ -11,3 +11,7 @@ print(f'So, you\'re {age} old, {height} feet tall, and pounds {weight} heavy.')
 
 emotion = input('Type how you are feeling in one word: ')
 print(f'So, you are feeling {emotion}.')
+
+favorite_num = int(input('What is your favorite number?'))
+half_favorite_num = favorite_num / 2
+print('So, your half favorite number is {}'.format(half_favorite_num))
